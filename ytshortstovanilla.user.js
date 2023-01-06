9bfece32d3b3321cf69faf69bdfcf176929b8360
Jordan Gilbreath <jgilbreath@buildtechnologygroup.com>
2023-01-05 20:27:35 -0600

    Increment version

--- ytshortstovanilla.user.js
+++ ytshortstovanilla.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Shorts To Vanilla Button
 // @namespace    https://jordanpg.dev/
-// @version      1.0
+// @version      1.1
 // @description  add a button to view youtube short in vanilla player
 // @author       jordanpg
 // @match        http*://www.youtube.com/shorts/*
